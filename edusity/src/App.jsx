615108c84539a70f93bc5903a8419b7673f34732
artem <artemfr@hotmail.com>
2024-04-01 01:24:29 +0200

    I'm adding and customizing diferent components like testimonials etc

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 const App = () => {
   return (
@@ -14,6 +16,10 @@ const App = () => {
         <Title subTitle='Our PROGRAM' title='What we offer'/>
         <Programs/>
         <About/>
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Campus/>
+        <Title subTitle='Testimonials' title='What students say'/>
+        <Testimonials/>
       </div>
     </div>
   )
---
+++ edusity/src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import './Testimonials.css'
+import next_icon from '/src/assets/next-icon.png'
+import back_icon from '/src/assets/back-icon.png'
+import user_1 from '/src/assets/user-1.png'
+import user_2 from '/src/assets/user-2.png'
+import user_3 from '/src/assets/user-3.png'
+import user_4 from '/src/assets/user-4.png'
+
+const Testimonials = () => {
+  return (
+    <div className='testimonials'>
+        <img src={next_icon} alt="" className='next-btn'/>
+        <img src={back_icon} alt="" className='back-btn'/>
+        <div className="slider">
+            <ul>
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_1} alt="" />
+                            <div>
+                                <h3>William Jackson</h3>
+                                <span>Edusity, USA</span>
+                            </div>
+                        </div>
+                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I&#39;ve ever made. The supportive community, state-f-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_2} alt="" />
+                            <div>
+                                <h3>William Jackson</h3>
+                                <span>Edusity, USA</span>
+                            </div>
+                        </div>
+                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I&#39;ve ever made. The supportive community, state-f-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_3} alt="" />
+                            <div>
+                                <h3>William Jackson</h3>
+                                <span>Edusity, USA</span>
+                            </div>
+                        </div>
+                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I&#39;ve ever made. The supportive community, state-f-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_4} alt="" />
+                            <div>
+                                <h3>William Jackson</h3>
+                                <span>Edusity, USA</span>
+                            </div>
+                        </div>
+                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I&#39;ve ever made. The supportive community, state-f-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonials
